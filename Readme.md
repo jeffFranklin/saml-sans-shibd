89e472ffc1b3467488254004b6431b6570fe0351
Jeff Franklin <jpf@uw.edu>
2018-02-17 20:38:32 -0800

    Initial commit.

---
+++ Readme.md
@@ -0,0 +1,30 @@
+# SAML sans shibd
+
+This project demonstrates a SAML login to UW's IdP without Shibboleth. This
+allows us to be a SAML SP without the added baggage of running shibd and
+apache mod_shib. This is a flask application running inside a docker container,
+and the key dependency is OneLogin's 
+[python3-saml package](https://github.com/onelogin/python3-saml).
+
+## Run instructions
+
+```
+echo '127.0.0.1   docker.internal' >> /etc/hosts
+openssl req -x509 -newkey rsa:4096 -nodes \
+	-out cert.pem -keyout key.pem -days 365
+docker run --name flaskapp --restart=always -p 443:443 -v `pwd`:/app \
+       -d jazzdd/alpine-flask:python3 -d
+```
+
+The last `-d` runs flask in debug mode, without nginx/uwsgi. This allows us to
+change our app on the fly, with the local changes being picked up and flask
+being restarted. This also run https directly from flask. The task of getting
+https working against nginx is an exercise left to the user.
+
+The initial `docker run` takes a couple minutes to install and build packages.
+You can tail the logs via `docker logs -f flaskapp` to see when it's ready.
+`docker stop flaskapp` when you're done.
+
+
+Now point your browser at `https://docker.internal`, authenticate, and 
+see your attributes.
---
+++ app.py
@@ -0,0 +1,73 @@
+import os
+from flask import request, session
+import flask
+from urllib.parse import urlparse
+from onelogin.saml2.auth import OneLogin_Saml2_Auth
+
+
+app = flask.Flask(__name__)
+app.config['SECRET_KEY'] = 'change this secret or else'
+app.config['SAML_PATH'] = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'saml')
+
+
+class FlaskSaml(OneLogin_Saml2_Auth):
+    def __init__(self, request):
+        """Init a OneLogin authentication object from a flask request."""
+        request_data = {
+            'https': 'on' if request.scheme == 'https' else 'off',
+            'http_host': request.host,
+            'server_port': urlparse(request.url).port,
+            'script_name': request.path,
+            'get_data': request.args.copy(),
+            'post_data': request.form.copy()
+        }
+        super().__init__(request_data, custom_base_path=app.config['SAML_PATH'])
+
+    def get_attributes(self):
+        """
+        Return a dict of SAML attributes, mapping then names to friendlier
+        ones we could use internally.
+        """
+        attribute_map = {
+            'urn:oid:0.9.2342.19200300.100.1.1': 'uwnetid',
+            'urn:oid:1.3.6.1.4.1.5923.1.1.1.1': 'affiliations',
+            'urn:oid:1.3.6.1.4.1.5923.1.1.1.6': 'eppn',
+            'urn:oid:1.3.6.1.4.1.5923.1.1.1.9': 'scopedAffiliations'
+        }
+        return {attribute_map.get(key, key): value
+                for key, value in super().get_attributes().items()}
+
+
+@app.route('/')
+def index():
+    attributes = list(session.get('samlAttributes', {}).items())
+    return flask.render_template('index.html', attributes=attributes)
+
+
+@app.route('/login')
+def login():
+    auth = FlaskSaml(request)
+    return flask.redirect(auth.login(return_to=request.host_url))
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return flask.redirect('/')
+
+
+@app.route('/Shibboleth.sso/SAML2/POST', methods=['POST'])
+def sso():
+    auth = FlaskSaml(request)
+    auth.process_response()
+    errors = auth.get_errors()
+    if errors:
+        raise Exception(errors)
+    session['samlAttributes'] = auth.get_attributes()
+    return flask.redirect(auth.redirect_to(request.form['RelayState']))
+
+
+if __name__ == "__main__":
+    sslctx = ('cert.pem', 'key.pem')
+    app.run(host='0.0.0.0', ssl_context=sslctx, port=443, debug=True)
+
